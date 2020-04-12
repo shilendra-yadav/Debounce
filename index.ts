81c5a1866c5b8d86d7dfa06e802f81ed65bf0858
shilendra12 <shilendra_yadav9@yahoo.com>
2020-04-13 02:20:59 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -27,3 +27,57 @@ var myThrottle = (cb, wait) => {
     }
   }
 }
+
+var CustomPromise = function (cb){
+  this.promiseChain = [];
+  this.handleError = () => {};
+  this.onResolve = this.onResolve.bind(this);
+  this.onReject = this.onResolve.bind(this);
+  cb(this.onResolve, this.onReject)
+}
+
+CustomPromise.prototype.then = function (onResolve){
+  this.promiseChain.push(onResolve);
+  return this;
+  
+}
+
+CustomPromise.prototype.onResolve = function (value) {
+  try {
+    let storedValue = value;
+    this.promiseChain.forEach(nextFunc => {
+      storedValue = nextFunc(storedValue);
+    });
+  } catch (error){
+    this.promiseChain = [];
+    this.onReject(error);
+  }
+}
+
+CustomPromise.prototype.catch = function (handleErrorCb){
+  this.handleError = handleErrorCb;
+  return this;
+}
+
+CustomPromise.prototype.onReject = function (error){
+  this.handleError(error);
+}
+
+// TESTING
+
+const pr = new CustomPromise((resolve, reject) => {
+	setTimeout(() => {
+		if (true) {
+			resolve("Success!!");
+		} else {
+			reject("Failure!");
+		}
+	}, 1000);
+});
+
+
+pr
+.then(result => result + "1")
+.then(prev => prev + "2")
+.then(prev => console.log(prev+"3"))
+.catch(error => console.log(error));
